fee01e35e3ca0b54f80c1578fcd7a95e8a81f7c1
vrosario01 <vrosario01@saintmarys.edu>
2026-09-10 07:30:54 -0400

    Update problem4.py

--- problem4.py
+++ problem4.py
@@ -3,7 +3,6 @@ def wears_jackets(temp, raining):
         return True
     else:
         return False
-
 """ wears_jackets takes in values temp and raining and only returns true if the temp < 60 or it is raining and returns false otherwise
 >>>print(wears_jackets(70, True))
 True
