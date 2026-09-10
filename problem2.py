aa8afe0324c866eb555551d5b069ce8bab1e8190
vrosario01 <vrosario01@saintmarys.edu>
2026-09-10 07:21:41 -0400

    Update problem2.py

--- problem2.py
+++ problem2.py
@@ -1,8 +1,13 @@
-
 def is_triangle(a,b,c):
-    if a + b < c and b + c < a and c + a < b:
+    if a + b > c and b + c > a and c + a > b:
         return True
     else:
         return False
-
-print(is_triangle(8,4,5))
+""" the function takes in values a b c and returns true if the triangle ineqaulities are met otherwise it returns false
+>>>print(is_triangle(3,4,5))
+True
+>>>print(is_triangle(4,7,10))
+True
+>>>print(is_triangle(5,3,10))
+False 
+"""
