3f8782dd31ee865ef11af8358413e5fe9c5e5e45
vrosario01 <vrosario01@saintmarys.edu>
2026-09-10 07:23:23 -0400

    Add files via upload

---
+++ problem3.py
@@ -0,0 +1,29 @@
+def print_range(start,stop,step):
+  while start < stop:
+    print(start)
+    start+=step
+""" this function takes in 3 values start stop and step. while start is less than stop the function prints all numbers plus step"""
+>>>print_range(1,10,2)
+3
+5
+7
+9
+>>>print_range(1,20,5)
+1
+6
+11
+16
+"""
+
+
+
+
+
+
+
+
+
+
+
+ 
+
