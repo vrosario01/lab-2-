c6728ebb5bc1d7ed1ef6494a17652352d6e74aab
vrosario01 <vrosario01@saintmarys.edu>
2026-09-11 18:50:17 -0400

    Update problem1.py

--- problem1.py
+++ problem1.py
@@ -1,9 +1,4 @@
 def pythagorean_triples( a, b, c):
-  if  a**2 + b**2 == c**2:
-    return True
-  else:
-    return False
-
 """ number^2 + number^2 = number^2  --> True
   pythagorean triples takes in parameter a b and c and if a^2 + b^2 = c^2 then returns true if not the function returns it false
 >>> print(pythagorean_triples(3,4,5))
@@ -13,4 +8,7 @@ False
 >>> print(pythagorean_triples(3,3,3))
 False
 """
-
+  if  a**2 + b**2 == c**2:
+    return True
+  else:
+    return False
